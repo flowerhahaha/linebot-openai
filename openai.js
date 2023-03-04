8699702c4e8d2dbd7e6a8d8a07747875adea6c4f
Yen <p12427@gmail.com>
2023-03-04 09:52:31 +0800

    feat: use gpt-3.5-turbo model for continuous conversation

--- openai.js
+++ openai.js
@@ -2,31 +2,35 @@
 const { Configuration, OpenAIApi } = require("openai")
 const chineseConv = require('chinese-conv')
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-  stream: true
+  apiKey: process.env.OPENAI_API_KEY
 })
 const openai = new OpenAIApi(configuration)
 
-let setting = ''
-// Process user input using OpenAI's API
+const MAX_MESSAGES_LENGTH = 10
+let messages = []
 const chatGPT = async (userInput) => {
-  console.log('userInput:', userInput)
+  // Check if the user needs to reset AI
   const match = userInput.match(/^Setting: (.+)/i)
-  setting = match ? `${match[1]}以下是我的訊息：` : setting
-  let prompt = match ? `${match[1]}` : `${setting}${userInput}。`
+  if (match) {
+    messages = [{role: "user", content:`${match[1]}`}]
+  } else {
+    messages.push({role: "user", content:`${userInput}`})
+  }
+  // If the length of the conversation exceeds 10 messages, delete the earliest message after the setting.
+  if (messages.length > MAX_MESSAGES_LENGTH) {
+    messages.splice(1, 1)
+  }
 
+  console.log('messages:', messages)
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: prompt,
-      temperature: 0.9,
-      max_tokens: 3000,
-      top_p: 1,
-      frequency_penalty: 0.5,
-      presence_penalty: 0.5,
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: messages,
     })
     // Clean up the text and translate simplified Chinese to traditional
-    const text = chineseConv.tify(completion.data.choices[0].text.trim().replace(/^[\n,.;:?!，。；：？！]+/, ''))
+    const text = chineseConv.tify(completion.data.choices[0].message.content.trim().replace(/^[\n,.;:?!，。；：？！]+/, ''))
+    // Save response records for continuous conversation
+    messages.push({role: "system", content:`${text}`})
     return text
   } catch (err) {
     console.log('error.message = ',err.message)
