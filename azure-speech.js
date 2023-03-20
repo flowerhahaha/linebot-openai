3404c4c0a2aca002524b09c05e201b46892b1bfa
DavidKK28 <127589236+DavidKK28@users.noreply.github.com>
2023-03-20 22:39:36 +0900

    transcript

--- azure-speech.js
+++ azure-speech.js
@@ -1,35 +1,78 @@
-const {
-  SpeechConfig,
-  AudioConfig,
-  SpeechRecognizer,
-  ResultReason
-} = require("microsoft-cognitiveservices-speech-sdk");
+const fs = require('fs');
+const { Buffer } = require('buffer');
+const path = require('path');
+const { SpeechConfig, AudioConfig, SpeechRecognizer, ResultReason, AudioInputStream } = require('microsoft-cognitiveservices-speech-sdk');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const ffmpegPath = process.env.NODE_ENV === 'production'
+  ? path.join(__dirname, 'ffmpeg', 'ffmpeg')
+  : require('@ffmpeg-installer/ffmpeg').path;
 
-const subscriptionKey = "const subscriptionKey = 'e33585f4decc46958be421161f45e15e";
+
+const subscriptionKey = "e33585f4decc46958be421161f45e15e";
 const serviceRegion = "eastus";
 
-const speechToText = async (audioFilePath) => {
-  return new Promise((resolve, reject) => {
-    const speechConfig = SpeechConfig.fromSubscription(subscriptionKey, serviceRegion);
-    const audioConfig = AudioConfig.fromWavFileInput(audioFilePath);
-    const recognizer = new SpeechRecognizer(speechConfig, audioConfig);
-
-    recognizer.recognized = (s, e) => {
-      if (e.result.reason === ResultReason.RecognizedSpeech) {
-        resolve(e.result.text);
-      } else {
-        reject("No speech recognized");
+const convertToWav = async (inputFilePath) => {
+    if (!fs.existsSync(inputFilePath)) {
+      throw new Error(`File not found: ${inputFilePath}`);
+    }
+  
+    const stats = fs.statSync(inputFilePath);
+    if (stats.size === 0) {
+      throw new Error(`File is empty: ${inputFilePath}`);
+    }
+  
+    const outputFilePath = path.resolve(`${Date.now()}.wav`);
+    const { stdout, stderr } = await exec(`${ffmpegPath} -i ${inputFilePath} -acodec pcm_s16le -ac 1 -ar 16000 ${outputFilePath}`);
+    return outputFilePath;
+  };
+  
+  const speechToText = async (audioFilePath) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const wavFilePath = await convertToWav(audioFilePath);
+        const audioData = fs.readFileSync(wavFilePath);
+        const speechConfig = SpeechConfig.fromSubscription(subscriptionKey, serviceRegion);
+        const audioStream = AudioInputStream.createPushStream();
+        audioStream.write(Buffer.from(audioData));
+        audioStream.close();
+        const audioConfig = AudioConfig.fromStreamInput(audioStream);
+        const recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+  
+        recognizer.recognizing = (s, e) => {
+          console.log(`RECOGNIZING: Text=${e.result.text}`);
+        };
+  
+        recognizer.recognized = (s, e) => {
+          fs.unlinkSync(wavFilePath); // 删除临时WAV文件
+          if (e.result.reason === ResultReason.RecognizedSpeech) {
+            console.log(`RECOGNIZED: Text=${e.result.text}`);
+            resolve(e.result.text);
+          } else if (e.result.reason === ResultReason.NoMatch) {
+            console.log("NOMATCH: Speech could not be recognized.");
+            reject('Speech could not be recognized.');
+          }
+        };
+  
+        recognizer.canceled = (s, e) => {
+          console.log(`CANCELED: Reason=${e.reason}`);
+          fs.unlinkSync(wavFilePath); // 删除临时WAV文件
+          reject('Speech recognition was canceled.');
+        };
+  
+        recognizer.sessionStopped = (s, e) => {
+          console.log("\n    Session stopped event.");
+          recognizer.stopContinuousRecognitionAsync();
+        };
+  
+        recognizer.startContinuousRecognitionAsync();
+      } catch (error) {
+        reject(`Error in speechToText: ${error}`);
       }
-    };
-
-    recognizer.canceled = (s, e) => {
-      reject(`Canceled: ${e.errorDetails}`);
-    };
-
-    recognizer.startContinuousRecognitionAsync();
-  });
-};
-
-module.exports = {
-  speechToText
-};
+    });
+  };
+  
+  module.exports = {
+    speechToText
+  };
+  
